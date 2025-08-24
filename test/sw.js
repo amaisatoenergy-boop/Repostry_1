97d014c318a5b6981c9e13f8944c3234941e84f9
amaisatoenergy-boop <amai.sato.energy@gmail.com>
2025-08-24 21:10:49 +0300

    Add files via upload

---
+++ test/sw.js
@@ -0,0 +1,18 @@
+﻿self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("ai-agent-v1").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "script.js",
+        "style.css",
+        "manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
